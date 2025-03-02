0430937c6cb7101728b643671094a6468e933cd9
Bala VJ <balavj@Balas-MacBook-Pro.local>
2025-03-02 05:50:02 +0530

    Fixed syntax error in Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -8,70 +8,64 @@ interface SignupProps {
 }
 
 const Signup: React.FC<SignupProps> = (props) => {
-{
-
-    const navigate = useNavigate()
-    const users = props.users 
-    const setusers = props.setusers
+    const navigate = useNavigate();
+    const users = props.users;
+    const setusers = props.setusers;
 
     const [eusername, setEusername] = useState<string>("");
     const [epassword, setEpassword] = useState<string>("");
 
-    const handleUInput = (evt: React.ChangeEvent<HTMLInputElement>) => 
-    {
-        setEusername(evt.target.value)
-    }
+    const handleUInput = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        setEusername(evt.target.value);
+    };
 
-    const handlePInput = (evt: React.ChangeEvent<HTMLInputElement>) => 
-    {
-        setEpassword(evt.target.value)
-    }
+    const handlePInput = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        setEpassword(evt.target.value);
+    };
 
-    const addUser = () => 
-    {
+    const addUser = () => {
         setusers([...users, { username: eusername, password: epassword }]);   
-        navigate("/")
-    }
-
+        navigate("/");
+    };
 
     return (
-    <div className="bg-black p-10">
-        <div className="bg-[#EFEFEF] p-10 border rounded-md">
-            <h1 className="text-3xl font-medium">Hey Hi</h1>
-            <p>Sign up here :)</p>
+        <div className="bg-black p-10">
+            <div className="bg-[#EFEFEF] p-10 border rounded-md">
+                <h1 className="text-3xl font-medium">Hey Hi</h1>
+                <p>Sign up here :)</p>
 
-            <div className="flex flex-col gap-2 my-2">
-                <input 
-                type="text" 
-                className="w-52 border-black p-1 bg-transparent border rounded-md" 
-                placeholder="username"
-                onChange={handleUInput}
-                />
+                <div className="flex flex-col gap-2 my-2">
+                    <input 
+                        type="text" 
+                        className="w-52 border-black p-1 bg-transparent border rounded-md" 
+                        placeholder="username"
+                        onChange={handleUInput}
+                    />
 
-<input 
-                type="text" 
-                className="w-52 border-black p-1 bg-transparent border rounded-md" 
-                placeholder="password"
-                onChange={handlePInput}
-                />
+                    <input 
+                        type="password" 
+                        className="w-52 border-black p-1 bg-transparent border rounded-md" 
+                        placeholder="password"
+                        onChange={handlePInput}
+                    />
 
-                <input 
-                    type="text" 
-                    className="w-52 border-black p-1 bg-transparent border rounded-md" 
-                    placeholder="confirm password"
-                />
+                    <input 
+                        type="password" 
+                        className="w-52 border-black p-1 bg-transparent border rounded-md" 
+                        placeholder="confirm password"
+                    />
 
+                    <button className="bg-[#FCA201] w-24 p-1 rounded-md" onClick={addUser}>
+                        Sign Up
+                    </button>
 
-                <button className="bg-[#FCA201] w-24 p-1 rounded-md" onClick={addUser}>
-                    Sign Up
-                </button>
-
-                <p>
-                    Already have an account? <Link to={"/"} className="underline">Login</Link>
-                </p>
+                    <p>
+                        Already have an account? <Link to={"/"} className="underline">Login</Link>
+                    </p>
+                </div>
             </div>
         </div>
-    </div>)
-}
+    );
+};
 
-export default Signup
+export default Signup;
